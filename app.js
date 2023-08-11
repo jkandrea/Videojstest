4566a292a898a01ef00fc1b5fa95e8345c29cee6
jaehun.jang <jhonbeetbox@naver.com>
2023-08-11 13:41:15 +0900

    watermark page add

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const GIFConverterCard = document.querySelector('#GIF-Converter-Card');
+const WaterMarkCard = document.querySelector('#WaterMark-Card');
 
 function movetogifconverter() {
     window.location.href = 'GIFConverter/gifconverter.html';
 }
 
+function movetowatermark() {
+    window.location.href = 'watermark.html';
+}
+
 GIFConverterCard.addEventListener('click', movetogifconverter);
+WaterMarkCard.addEventListener('click', movetowatermark);
