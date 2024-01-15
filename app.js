4dda5531dbbf53cd210c7afb99bf138566342317
jaehun.jang <jhonbeetbox@naver.com>
2024-01-15 16:43:06 +0900

    move blog thumbnail

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const GIFConverterCard = document.querySelector('#GIF-Converter-Card');
 const WaterMarkCard = document.querySelector('#WaterMark-Card');
+const BlogThumbnailCard = document.querySelector('#Blogthumbnail-Card');
 
 function movetogifconverter() {
     window.location.href = 'GIFConverter/gifconverter.html';
@@ -9,5 +10,10 @@ function movetowatermark() {
     window.location.href = 'watermark.html';
 }
 
+function movetoblogthumbnail() {
+    window.location.href = 'blogthumbnail.html';
+}
+
 GIFConverterCard.addEventListener('click', movetogifconverter);
 WaterMarkCard.addEventListener('click', movetowatermark);
+BlogThumbnailCard.addEventListener('click', movetoblogthumbnail);
